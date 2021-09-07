042045815e281e27e04debb081454f7e8ce2cb73
sairahul1526 <dravid.rahul1526@gmail.com>
2021-09-07 10:26:12 +0530

    removed secrets and new push

---
+++ email/constant/email.go
@@ -0,0 +1,7 @@
+package constant
+
+// config
+const (
+	FromEmailIDTransactional = "SAL Foundation <alerts@sal.foundation>"
+	FromEmailIDPromotional   = "SAL Foundation <offers@sal.foundation>"
+)
---
+++ message/constant/message.go
@@ -0,0 +1,7 @@
+package constant
+
+// config
+const (
+	CorefactorsSendSMSEndpoint = "https://teleduce.corefactors.in/sendsms/"
+	TextMessageFrom            = "SALUBR"
+)
